a7dd0d51a063022c0019e964a968b64a71b0a681
Lucywuinwa <lucywuinwa@gmail.com>
2024-01-25 18:52:08 -0800

    Revert "ML"
    This reverts commit c919ddf546236ce02f53ee8f6513b4cdcffc2d89.

--- Python/duckgoose.py
+++
@@ -1,8 +0,0 @@
-import sys
-duck_goose = sys.argv[1:]
-
-def remove_goose(var):
-    new_list = [item for item in duck_goose if item != 'goose']
-    return new_list
-
-print(remove_goose(sys.argv[1]))
